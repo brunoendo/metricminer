5bb5f32a966460dab5684f57a62cab87f10b86e2
Renan Reis <renanreismartins@gmail.com>
2013-04-09 22:05:44 -0300

    implementando a factory da metrica e adicionando o result nas confs do hibernate

--- src/main/java/org/metricminer/tasks/metric/staticcounter/StaticCounterMetricFactory.java
+++ src/main/java/org/metricminer/tasks/metric/staticcounter/StaticCounterMetricFactory.java
@@ -1,5 +1,15 @@
 package org.metricminer.tasks.metric.staticcounter;
 
-public class StaticCounterMetricFactory {
+import org.metricminer.tasks.MetricComponent;
+import org.metricminer.tasks.metric.common.Metric;
+import org.metricminer.tasks.metric.common.MetricFactory;
+
+@MetricComponent(name="Quantity of Static Attributes and Methods")
+public class StaticCounterMetricFactory implements MetricFactory {
+
+	@Override
+	public Metric build() {
+		return new StaticCounterMetric();
+	}
 
 }
