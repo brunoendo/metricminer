154af10c3baff5a7ebc8715e93efbde837865316
Mauricio Aniche <mauricioaniche@gmail.com>
2013-04-15 16:15:43 -0300

    desligando a metrica, pro mm nao rodar

--- src/main/java/org/metricminer/tasks/metric/unitorsystemtest/UnitOrSystemTestFactory.java
+++ src/main/java/org/metricminer/tasks/metric/unitorsystemtest/UnitOrSystemTestFactory.java
@@ -4,7 +4,7 @@ import org.metricminer.tasks.MetricComponent;
 import org.metricminer.tasks.metric.common.Metric;
 import org.metricminer.tasks.metric.common.MetricFactory;
 
-@MetricComponent(name="Unit or System Test Identifier")
+//@MetricComponent(name="Unit or System Test Identifier")
 public class UnitOrSystemTestFactory implements MetricFactory {
 
 	@Override
