2ae5320a9036c9477055bdf60b9172a5fb98cdcd
Mauricio Aniche <mauricioaniche@gmail.com>
2013-09-02 00:03:35 -0300

    testes de correlacao

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -8,3 +8,7 @@ INSERT INTO `StatisticalTest` VALUES (2,'set1 <- #set1#\nset2 <- #set2#\nt1 = wi
 INSERT INTO `StatisticalTest` VALUES (3,'set1 <- #set1#\nset2 <- #set2#\nt1 = t.test(set1, set2, paired = TRUE)\nt1','Paired T-Student Test',NULL);
 
 INSERT INTO `StatisticalTest` VALUES (4,'set1 <- #set1#\nset2 <- #set2#\nt1 = t.test(set1, set2, paired = FALSE)\nt1','Non-Paired T-Student Test',NULL);
+
+INSERT INTO `StatisticalTest` VALUES (5,'set1 <- #set1#\nset2 <- #set2#\nt1 = cor.test(set1, set2, method="pearson")\nt1','Pearson Correlation',NULL);
+
+INSERT INTO `StatisticalTest` VALUES (6,'set1 <- #set1#\nset2 <- #set2#\nt1 = cor.test(set1, set2, method="spearman")\nt1','Spearman Correlation',NULL);
